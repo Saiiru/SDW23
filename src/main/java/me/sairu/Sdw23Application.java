f19c6f5a69fe8c7f40c2eba855dcbc97435a999b
[SAIRU] <[sairufsa@gmail.com]>
2024-06-10 14:28:14 -0300

    First Commit

---
+++ src/main/java/me/sairu/Sdw23Application.java
@@ -0,0 +1,13 @@
+package me.sairu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdw23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdw23Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/sairu/Sdw23ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sairu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdw23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
