946f92924b9fcd747344371e737d78e890675c8d
[SAIRU] <[sairufsa@gmail.com]>
2024-06-11 18:14:16 -0300

    Fix repository error

--- src/main/java/me/sairu/domain/repository/UserRepository.java
+++ src/main/java/me/sairu/domain/repository/UserRepository.java
@@ -1,15 +1,12 @@
 package me.sairu.domain.repository;
 
 import me.sairu.domain.model.User;
+import me.sairu.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
     boolean existsByAccountNumber(String number);
-
     boolean existsByCardNumber(String number);
 }
